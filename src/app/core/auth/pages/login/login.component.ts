78f9aa8105056dd8c425b06c617c2234b082cc26
aliimohamed <aliimohamed2003@gmail.com>
2026-02-24 02:27:58 +0200

    Updated

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule, RouterLink],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
+      ]),
+    });
+  }
+  Login() {
+    console.log(this.loginForm.value);
+    this.ClearForm();
+  }
+  ClearForm() {
+    const today = new Date();
+    this.loginForm.reset({
+      email: '',
+      password: '',
+    });
+  }
 
   ngOnInit() {}
 }
