f621175088d674eb0023381e245b67d06e9c2060
aliimohamed <aliimohamed2003@gmail.com>
2026-02-25 21:38:03 +0200

    updated

--- src/app/core/auth/pages/forget-password/forget-password.component.ts
+++ src/app/core/auth/pages/forget-password/forget-password.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoaderComponent } from '../../../layouts/components/loader/loader.component';
 
 @Component({
   selector: 'app-forget-password',
   templateUrl: './forget-password.component.html',
   styleUrls: ['./forget-password.component.css'],
+  imports: [ReactiveFormsModule],
 })
 export class ForgetPasswordComponent implements OnInit {
-  constructor() {}
-
+  resetPasswordForm: FormGroup;
+  constructor() {
+    this.resetPasswordForm = new FormGroup({
+      password: new FormControl('', Validators.required),
+      newPassword: new FormControl('', Validators.required),
+    });
+  }
+  resetPassword() {
+    console.log(this.resetPasswordForm.value);
+    this.clearForm();
+  }
+  clearForm() {
+    this.resetPasswordForm.reset({
+      password: '',
+      newPassword: '',
+    });
+  }
   ngOnInit() {}
 }
